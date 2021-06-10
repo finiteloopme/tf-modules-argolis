56711778b77abe1f5e0b2e632ecafd5729b98ed9
Kunal Limaye <kunall@google.com>
2021-06-11 09:19:53 +1000

    Reworded descriptions

--- modules/gce/outputs.tf
+++ modules/gce/outputs.tf
@@ -1,11 +1,11 @@
-output "instances_self_links" {
-  description = "List of self-links for compute instances"
+output "instance_self_link" {
+  description = "Self link for the instance"
   value       = module.compute_instance.instances_self_links
   sensitive   = true
 }
 
 output "instance_details" {
-  description = "List of details for compute instances"
+  description = "GCE instance details"
   value       = module.compute_instance.instances_details
   sensitive   = true
 }
