d681145f78c5a4843b8ef113b80dfc57678fbda3
Kunal Limaye <kunall@google.com>
2021-06-11 09:17:23 +1000

    Adding sensitive=true flag

--- modules/gce/data.tf
+++ modules/gce/data.tf
@@ -6,13 +6,7 @@ data "google_compute_default_service_account" "default_sa" {
     project         = data.google_project.gcp_project.project_id
 }
 
-# data "google_compute_network" "default_network" {
-#   name = var.network_name
-# }
-
 data "google_compute_subnetwork" "vm_subnetwork" {
-  # self_link         = "projects/kunal-scratch/regions/us-central1/subnetworks/default"
-  # name            = "${var.subnetwork_id}-${var.gcp_region}"
   name            = var.subnetwork_id
   region          = var.gcp_region
   project         = data.google_project.gcp_project.project_id
--- modules/gce/outputs.tf
+++ modules/gce/outputs.tf
@@ -1,9 +1,10 @@
 output "instances_self_links" {
   description = "List of self-links for compute instances"
   value       = module.compute_instance.instances_self_links
+  sensitive   = true
 }
 
-output "instances_details" {
+output "instance_details" {
   description = "List of details for compute instances"
   value       = module.compute_instance.instances_details
   sensitive   = true
