a5fa0935bcfad6d7352d09904bde74691de49b3d
Kunal Limaye <kunall@google.com>
2021-06-16 15:58:24 +1000

    Updating TF module to account for multiple gke clusters

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -11,9 +11,9 @@ module "gke-subnet"{
   source            = "git::https://github.com/finiteloopme/tf-modules-argolis.git//modules/subnet"
   project_id                  = var.project_id
   network                     = var.network
-  gke_network                 = var.gke_instance.name
-  ip_range_pods               = var.secondary_ranges.pod_ips.name
-  svc_ips                     = var.secondary_ranges.svc_ips.name
+  gke_network                 = var.gke_instance.subnet.name
+  ip_range_pods               = var.gke_instance.secondary_ranges.pod_ips.name
+  svc_ips                     = var.gke_instance.secondary_ranges.svc_ips.name
 
 }
 
@@ -27,13 +27,13 @@ module "gke" {
   regional                    = true # Create a regional cluster
   region                      = var.gcp_region
   # zones                       = [var.gcp_zone]
-  network                     = var.gke_instance.name
+  network                     = var.gke_instance.subnet.name
   # subnetwork                  = var.subnetwork
-  subnetwork                  = module.gke-subnet.subnets["${var.gcp_region}/${var.gke_network.name}"].name
+  subnetwork                  = module.gke-subnet.subnets["${var.gcp_region}/${var.gke_instance.subnet.name}"].name
   # subnetwork                  = var.gke_network.name
 
-  ip_range_pods               = var.econdary_ranges.pod_ips.name
-  ip_range_services           = var.secondary_ranges.svc_ips.name
+  ip_range_pods               = var.gke_instance.secondary_ranges.pod_ips.name
+  ip_range_services           = var.gke_instance.secondary_ranges.svc_ips.name
 
   create_service_account      = false
   # service_account             = var.compute_engine_service_account
