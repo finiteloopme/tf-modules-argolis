744842b6c3b9b690f85582d980e4d3c07b52bf9c
Kunal Limaye <kunall@google.com>
2021-07-12 14:47:01 +1000

    Removing explicit remove

--- modules/hosted-config-connector/main.tf
+++ modules/hosted-config-connector/main.tf
@@ -23,7 +23,7 @@ module "project-services" {
 ##################################################################
 # 3. Config connector module
 ##################################################################
-resource "null_resource" "config-contoller-create"{
+resource "null_resource" "config-contoller"{
 
   triggers = {
     project_id          = data.google_project.host_project.project_id
@@ -36,21 +36,26 @@ resource "null_resource" "config-contoller-create"{
         command         = "${path.module}/scripts/install-hosted-config-connector.sh  ${self.triggers.project_id} ${self.triggers.config_connecor_id} ${self.triggers.config_connector_region}"
     }
 
+    provisioner "local-exec" {
+        when            = destroy
+        command         = "${path.module}/scripts/uninstall-hosted-config-connector.sh ${self.triggers.project_id} ${self.triggers.config_connecor_id} ${self.triggers.config_connector_region}"
+    }
+
     depends_on = [
         module.project-services,
     ]
 }
 
-resource "null_resource" "config-contoller-remove"{
+# resource "null_resource" "config-contoller-remove"{
 
-  triggers = {
-    project_id          = data.google_project.host_project.project_id
-    config_connecor_id  = var.config_connecor_id
-    config_connector_region = var.config_connector_region
-  }
+#   triggers = {
+#     project_id          = data.google_project.host_project.project_id
+#     config_connecor_id  = var.config_connecor_id
+#     config_connector_region = var.config_connector_region
+#   }
 
-    provisioner "local-exec" {
-        when            = destroy
-        command         = "${path.module}/scripts/uninstall-hosted-config-connector.sh ${self.triggers.project_id} ${self.triggers.config_connecor_id} ${self.triggers.config_connector_region}"
-    }
-}
+#     provisioner "local-exec" {
+#         when            = destroy
+#         command         = "${path.module}/scripts/uninstall-hosted-config-connector.sh ${self.triggers.project_id} ${self.triggers.config_connecor_id} ${self.triggers.config_connector_region}"
+#     }
+# }
