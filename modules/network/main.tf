4fa7df20163fbaa956b84450107a8b31d29b4f9b
Kunal Limaye <kunall@google.com>
2021-10-16 15:02:57 +1100

    Add FR to allow internal traffic

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -48,3 +48,41 @@ module "firewall-rule-allow-ssh-http-s" {
     module.default-network
   ]
 }
+
+# Create Firewall rule to allow internal 
+module "firewall-rule-default-allow-internal" {
+  source       = "terraform-google-modules/network/google//modules/firewall-rules"
+  project_id   = data.google_project.current_gcp_project.project_id
+  network_name = var.network_name
+
+  rules = [{
+    name                    = "default-allow-internal"
+    description             = "Allow Internal traffic"
+    direction               = "INGRESS"
+    priority                = null
+    ranges                  = ["10.128.0.0/9"]
+    source_tags             = null
+    source_service_accounts = null
+    target_tags             = null
+    target_service_accounts = null
+    allow = [{
+      protocol = "tcp"
+      ports    = ["22-65535"]
+    },
+    {
+      protocol = "udp"
+      ports    = ["22-65535"]
+    },
+    {
+      protocol = "icmp"
+    }]
+    deny = []
+    log_config = {
+      metadata = "INCLUDE_ALL_METADATA"
+    }
+  }]
+
+  depends_on  = [
+    module.default-network
+  ]
+}
