bebd1dfd622a52a4206672295638a0dff4b5bc46
Kunal L <kunallimaye@gmail.com>
2023-08-15 09:11:11 +1000

    Enable output var

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,8 +1,8 @@
-# output "subnets" {
-#     value = module.default-network.subnets_self_links
-# }
+output "subnets" {
+    value = module.default-network.subnets_self_links
+}
 
-# output "network" {
-#     value = module.default-network.network_self_link
-# }
+output "network" {
+    value = module.default-network.network_self_link
+}
 
